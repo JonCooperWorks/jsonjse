2379d6738ff4454ae23a305a5f1682e20c6ee82a
Jonathan Cooper <cooper@cooperthecoder.com>
2020-05-25 23:40:35 -0500

    Added CORS

--- routes.go
+++ routes.go
@@ -4,6 +4,9 @@ import "github.com/gin-gonic/gin"
 
 func Router(config *ServerConfig) *gin.Engine {
 	router := gin.Default()
+	router.Use(func (c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+	})
 
 	handlers := &JSEHandlers{ServerConfig: config}
 	stocks := router.Group("/jse")
